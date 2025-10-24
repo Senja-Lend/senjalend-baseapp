d36de5ca797c28015c41cf9e6e314bddc721fcf7
ahmadzz <royanuahmad313@gmail.com>
2025-10-25 01:23:01 +0700

    refactor: Update token addresses and remove obsolete Ethereum entry in tokenAddress.ts

--- src/lib/addresses/tokenAddress.ts
+++ src/lib/addresses/tokenAddress.ts
@@ -2,25 +2,14 @@ import { Token } from "@/types";
 
 export const tokens: Token[] = [
   {
-    name: "Ethereum",
-    symbol: "ETH",
-    logo: "/token/ethereum.png",
-    decimals: 18,
-    oftAddress: "0x15858A57854BBf0DF60A737811d50e1Ee785f9bc",
-    addresses: {
-      8453: "0x0000000000000000000000000000000000000001",
-      42161: "0x0000000000000000000000000000000000000001",
-    },
-  },
-  {
     name: "WETH",
     symbol: "WETH",
     logo: "/token/weth.png",
     decimals: 18,
-    oftAddress: "0x007F735Fd070DeD4B0B58D430c392Ff0190eC20F", 
+    oftAddress: "0x08F17aC3389f8A719602681747F668fa4EFdBbAb",
     addresses: {
-      8453: "0x7954270F038BFaE7760cCF8D9094745d3E9cf4A3", //Mweth
-      42161: "0x4200000000000000000000000000000000000006",  //weth
+      8453: "0x4200000000000000000000000000000000000006", //weth
+      42161: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", //weth
     },
   },
   {
@@ -28,10 +17,10 @@ export const tokens: Token[] = [
     symbol: "WBTC",
     logo: "/token/wbtc.png",
     decimals: 8,
-    oftAddress: "0x4Ba8D8083e7F3652CCB084C32652e68566E9Ef23",
+    oftAddress: "0x7bFA71D5f8A3BfA35384E14863E299Ef20B2652a",
     addresses: {
       8453: "0x0555E30da8f98308EdB960aa94C0Db47230d2B9c", //wbtc
-      42161: "0x68f180fcCe6836688e9084f035309E29Bf0A2095", //wbtc
+      42161: "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f", //wbtc
     },
   },
   {
@@ -39,10 +28,10 @@ export const tokens: Token[] = [
     symbol: "USDT",
     logo: "/token/usdt.png",
     decimals: 6,
-    oftAddress: "0x5Bf04cc43cd55167eB0FEF1cD6Cd15Da48C52490",
+    oftAddress: "0x90a07246df6cc23C26343Fc4595aC71e834dcfd3",
     addresses: {
-      8453: "0xd61F31154bF292c7bE2fD81fAc9810f6d93Ecc2B", //Musdt
-      42161: "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58", // usdt
+      8453: "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2", //usdt
+      42161: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9", // usdt
     },
   },
   {
@@ -50,10 +39,10 @@ export const tokens: Token[] = [
     symbol: "USDC",
     logo: "/token/usdc.png",
     decimals: 6,
-    oftAddress: "0xdF05e9AbF64dA281B3cBd8aC3581022eC4841FB2",
+    oftAddress: "0x4DbfE8f6a12E3538e4F2B3712ec81cD7A161e3bd",
     addresses: {
-      8453: "0xD2E0F459A2518b9459B9b11dB5Aa014F0BF622A7",//usdc
-      42161: "0xfBC915dc39654b52B2E9284FB966C79A1071eA3A",
+      8453: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913", //usdc
+      42161: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831", // usdc
     },
   },
 ];
